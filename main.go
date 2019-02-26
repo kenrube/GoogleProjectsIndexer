82325fa3f110fcac5cfc4c9be200acc211c73229
kenrube <kenrube@gmail.com>
2019-02-26 10:08:11 +0300

    Extract classes from all libraries

--- main.go
+++ main.go
@@ -6,21 +6,29 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"strconv"
 
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
-	"github.com/gocolly/colly/queue"
 )
 
+type Library struct {
+	Name string
+	Link string
+}
+
+type LibraryClasses struct {
+	Name    string     `json:"library_name"`
+	Classes []ApiClass `json:"classes"`
+}
+
 type ApiClass struct {
-	Index               int    `json:"index"`
+	Id                  int    `json:"id"`
 	Name                string `json:"name"`
 	NameExtended        string `json:"name_extended"`
 	Link                string `json:"link"`
 	Description         string `json:"description"`
-	AddedInVersion      int    `json:"added_in_version"`
-	DeprecatedInVersion int    `json:"deprecated_in_version"`
+	AddedInVersion      string `json:"added_in_version"`
+	DeprecatedInVersion string `json:"deprecated_in_version"`
 }
 
 func main() {
@@ -31,46 +39,40 @@ func main() {
 
 	extensions.RandomUserAgent(c)
 
-	q, err := queue.New(8, nil)
-	check(err)
+	var id int
+	var libraryClasses []LibraryClasses
+	libraries := [...]Library{
+		{Name: "Android Platform", Link: "/classes"},
+		{Name: "Android Support Library", Link: "/android/support/classes"},
+		{Name: "AndroidX", Link: "/androidx/classes"},
+		{Name: "AndroidX Test", Link: "/androidx/test/classes"},
+		{Name: "Architecture Components", Link: "/android/arch/classes"},
+		{Name: "Android Automotive Library", Link: "/android/car/classes"},
+		{Name: "Databinding Library", Link: "/android/databinding/classes"},
+		{Name: "Constraint Layout Library", Link: "/android/support/constraint/classes"},
+		{Name: "Material Components", Link: "/com/google/android/material/classes"},
+		{Name: "Test Support Library", Link: "/android/support/test/classes"},
+		{Name: "Wearable Library", Link: "/android/support/wearable/classes"},
+		{Name: "Play Billing Library", Link: "/com/android/billingclient/classes"},
+		{Name: "Play Core Library", Link: "/com/google/android/play/core/classes"},
+		{Name: "Play Install Referrer Library", Link: "/com/android/installreferrer/classes"},
+		{Name: "Android Things", Link: "/com/google/android/things/classes"},
+	}
 
-	var index int
-	var apiClasses []ApiClass
-
-	c.OnHTML("tr[data-version-added]", func(e *colly.HTMLElement) {
-		index++
-		name := e.DOM.Find("td[class=jd-linkcol]>a[href]").First().Text()
-		nameExtended := e.ChildText("td[class=jd-linkcol]")
-		link := e.ChildAttr("td[class=jd-linkcol]>a[href]", "href")
-		description := e.ChildText("td[class=jd-descrcol]")
-		description = regexp.MustCompile("\\s{2,}").ReplaceAllString(description, " ")
-		addedInVersion, _ := strconv.Atoi(e.Attr("data-version-added"))
-		deprecatedInVersion, _ := strconv.Atoi(e.Attr("data-version-deprecated"))
-
-		apiClass := ApiClass{
-			Index:               index,
-			Name:                name,
-			NameExtended:        nameExtended,
-			Link:                link,
-			Description:         description,
-			AddedInVersion:      addedInVersion,
-			DeprecatedInVersion: deprecatedInVersion,
-		}
-		apiClasses = append(apiClasses, apiClass)
-
-		/*err := q.AddURL(link)
-		check(err)*/
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		id++
+		libraryClasses[len(libraryClasses)-1].Classes =
+			append(libraryClasses[len(libraryClasses)-1].Classes, parseApiClass(id, e))
 	})
 
-	// a[class=__asdk_search_extension_link__]
+	for index := range libraries {
+		libraryClasses = append(libraryClasses, LibraryClasses{libraries[index].Name, []ApiClass{}})
+		// TODO visit links concurrently
+		err := c.Visit("https://developer.android.com/reference" + libraries[index].Link)
+		check(err)
+	}
 
-	err = q.AddURL("https://developer.android.com/reference/classes")
-	check(err)
-
-	err = q.Run(c)
-	check(err)
-
-	jsonData, err := json.MarshalIndent(apiClasses, "", "    ")
+	jsonData, err := json.MarshalIndent(libraryClasses, "", "    ")
 	check(err)
 
 	err = ioutil.WriteFile("classes_index.json", jsonData, os.ModePerm)
@@ -78,8 +80,28 @@ func main() {
 	log.Println(string(jsonData))
 }
 
+func parseApiClass(id int, e *colly.HTMLElement) ApiClass {
+	name := e.DOM.Find("td[class=jd-linkcol]>a[href]").First().Text()
+	nameExtended := e.ChildText("td[class=jd-linkcol]")
+	link := e.ChildAttr("td[class=jd-linkcol]>a[href]", "href")
+	description := e.ChildText("td[class=jd-descrcol]")
+	description = regexp.MustCompile("\\s{2,}").ReplaceAllString(description, " ")
+	addedInVersion := e.Attr("data-version-added")
+	deprecatedInVersion := e.Attr("data-version-deprecated")
+
+	return ApiClass{
+		Id:                  id,
+		Name:                name,
+		NameExtended:        nameExtended,
+		Link:                link,
+		Description:         description,
+		AddedInVersion:      addedInVersion,
+		DeprecatedInVersion: deprecatedInVersion,
+	}
+}
+
 func check(err error) {
 	if err != nil {
-		log.Fatal()
+		log.Println(err)
 	}
 }
